0068c4da2f18e419db5b2daa58e940e8a5fbc963
JoseFredes <jose.fredes@mayor.cl>
2024-01-08 17:34:03 -0300

    feat: add localhost route

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://fsma-8x5kvs6xx-josefredes.vercel.app/',
+    origin: [
+      'https://fsma-8x5kvs6xx-josefredes.vercel.app/',
+      'http://localhost:5173/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
