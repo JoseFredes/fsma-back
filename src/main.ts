2b78486e42be8cc33f57918d7b28de42ff91b129
JoseFredes <jose.fredes@mayor.cl>
2024-01-08 17:29:48 -0300

    feat: Add cors package for enabling Cross-Origin Resource Sharing (CORS)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://fsma-8x5kvs6xx-josefredes.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
